548b91cd0c88ab1e3ef76bf604ba606b165d88f8
Aaron VonderHaar <av@noredink.com>
2017-03-22 14:01:08 -0700

    Format code with prettier

--- tests/mainSpec.js
+++ tests/mainSpec.js
@@ -1,39 +1,32 @@
-
 function parse(input) {
-  if (input.split('\n').length > 2) {
+  if (input.split("\n").length > 2) {
     return [
       {
-        name: 'First line',
+        name: "First line",
         children: [
-          {name: 'sub 1', children: []},
-          {name: 'sub 2', children: []}
+          { name: "sub 1", children: [] },
+          { name: "sub 2", children: [] }
         ]
       }
     ];
   } else {
-    return [
-      {name: 'First line', children: []}
-    ];
+    return [{ name: "First line", children: [] }];
   }
 }
 
-
-
-describe('example', function() {
-  it('should parse single item', function() {
-    var result = parse('- First line\n');
-    expect(result).toEqual([
-      {name: 'First line', children: []}
-    ]);
+describe("example", function() {
+  it("should parse single item", function() {
+    var result = parse("- First line\n");
+    expect(result).toEqual([{ name: "First line", children: [] }]);
   });
-  it('should parse children', function() {
-    var result = parse('- First line\n  -sub 1\n  -sub2\n');
+  it("should parse children", function() {
+    var result = parse("- First line\n  -sub 1\n  -sub2\n");
     expect(result).toEqual([
       {
-        name: 'First line',
+        name: "First line",
         children: [
-          {name: 'sub 1', children: []},
-          {name: 'sub 2', children: []}
+          { name: "sub 1", children: [] },
+          { name: "sub 2", children: [] }
         ]
       }
     ]);
